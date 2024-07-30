ac25eca583f2653568eca5fa829765b574e0a17d
GirishNekar <gmnekar18@gmail.com>
2024-07-30 23:13:08 +0530

    [Girish : committed first_last program]

---
+++ Basic_Problem/first_last.py
@@ -0,0 +1,42 @@
+
+
+'''
+
+@Author: Girish
+@Date: 2024-07-24 
+@Last Modified by: Girish
+@Last Modified time: 2024-07-24 
+@Title : Finding day of a week for given date
+
+'''
+
+
+def print_first_last(color_list):
+    
+    
+    """
+    
+    Description:
+        prints first element and last element in the list 
+         
+    Parameter:
+        none
+        
+    Return:
+        none
+         
+
+    """
+    
+    
+    print(f'First COlour in the list is {color_list[0]} and last colour in the list is {color_list[1]}')
+
+
+def main():
+       
+    color_list = ["Red","Green","White" ,"Black"]
+    print_first_last(color_list) 
+    
+
+if __name__ == "__main__":
+    main()
